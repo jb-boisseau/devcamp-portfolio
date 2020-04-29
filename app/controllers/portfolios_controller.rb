8e41f412d4a424558187d45d8c902c14832a4735
JB Boisseau <jb.boisseau@mesvaccins.net>
2020-04-29 11:57:07 +0200

    implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,17 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    #perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    #Destroy/delete the record
+    @portfolio_item.destroy
+
+    #redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio was removed."}
+    end
+  end
+
 end
