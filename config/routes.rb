1edf02350ca9d817432f98878b558c3f9eff8240
JB Boisseau <jb.boisseau@mesvaccins.net>
2020-04-30 12:11:20 +0200

    implemented toggle status published

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get 'about-me', to:'pages#about'
   get 'contact-me', to:'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   root to: "pages#home"
 end
