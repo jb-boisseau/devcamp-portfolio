a6ca7f365dbc6de95bc56bc69bb558c9a0bb4956
JB Boisseau <jb.boisseau@mesvaccins.net>
2020-05-05 14:24:20 +0200

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,12 +27,19 @@ end
      thumb_image: "http://placehold.it/300x200"
    )
  end
- 1.times do | portfolio_item |
-   Portfolio.create!(
-     title: "Portfolio title #{portfolio_item}" ,
-     subtitle: "Angular",
-     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-     main_image: "http://placehold.it/600x400",
-     thumb_image: "http://placehold.it/300x200"
+1.times do | portfolio_item |
+ Portfolio.create!(
+   title: "Portfolio title #{portfolio_item}" ,
+   subtitle: "Angular",
+   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+   main_image: "http://placehold.it/600x400",
+   thumb_image: "http://placehold.it/300x200"
+ )
+end
+
+3.times do |technology|
+   Portfolio.last.technologies.create!(
+     name:"Technology #{technology}"
    )
- end
+end
+puts "3 tech added"
