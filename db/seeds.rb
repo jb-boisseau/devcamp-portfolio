836e57072eda429f6c7617a8894ea890379a0799
JB Boisseau <jb.boisseau@mesvaccins.net>
2020-05-05 10:57:42 +0200

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+puts "3 topics created"
+
 10.times do | blog |
   Blog.create!(
     title: "My blog post #{blog}",
-    body: "#{blog} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    body: "#{blog} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    topic_id: Topic.last.id
   )
 end
  5.times do | skill |
@@ -10,10 +18,19 @@ end
     percent_utilized: 7 * skill.to_i
    )
 end
- 9.times do | portfolio_item |
+ 8.times do | portfolio_item |
    Portfolio.create!(
      title: "Portfolio title #{portfolio_item}" ,
-     subtitle: "My advice",
+     subtitle: "Ruby on Rails",
+     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+     main_image: "http://placehold.it/600x400",
+     thumb_image: "http://placehold.it/300x200"
+   )
+ end
+ 1.times do | portfolio_item |
+   Portfolio.create!(
+     title: "Portfolio title #{portfolio_item}" ,
+     subtitle: "Angular",
      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
      main_image: "http://placehold.it/600x400",
      thumb_image: "http://placehold.it/300x200"
