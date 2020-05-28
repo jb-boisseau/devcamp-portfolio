5267294f5e76f25a25abca8fcf7f201bf1a42a90
JB Boisseau <jb.boisseau@mesvaccins.net>
2020-05-28 10:13:04 +0200

    integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,11 @@ class ApplicationController < ActionController::Base
     @copyright = JbboisseauViewTool::Renderer.copyright 'JB Boisseau', 'Tous droits réservés'
   end
 end
+
+module JbboisseauViewTool
+  class Renderer
+    def self.copyright name, msg
+      "<kbd> &copy; #{Time.now.year} | <b>#{name}</b> #{msg} </kbd".html_safe
+    end
+  end
+end
